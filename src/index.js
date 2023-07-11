fefa0825726ab97cf872e1e6206429696ae1cbf0
Kevo-gmm <61180578+Kevo-gmm@users.noreply.github.com>
2023-07-11 20:09:45 +0300

    resolved imports issues

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import App from './App'
-import './index.css'
-import store from './store'
+import React from "react";
 
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-)
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import App from './App'
-import './index.css'
-import store from './store'
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import store from "./store";
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 
 createRoot(rootElement).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>
-)
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
