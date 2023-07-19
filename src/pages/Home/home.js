b8cfd8e014954e53041fd9531156dee2c40c702f
CarolMainaa <carolmaina502@gmail.com>
2023-07-20 01:33:03 +0300

    changes

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react'
-import SearchForm from '../../container/search-form/search-form'
-import image from '../../img/homepage.jpg'
-import './style.css'
-import { Header } from '../../components/header/header'
-import { BookingPage } from '../booking/booking'
+import React, { useState } from "react";
+import { Header } from "../../components/header/header";
+import SearchForm from "../../container/search-form/search-form";
+import image from "../../img/homepage.jpg";
+import "./style.css";
+import FlightInfo from "../../components/flight-info/flight-info";
 
 export const Home = () => {
-	const [flights, setFlights] = useState([])
-	console.log('========================FLIGHTS================')
-	console.log(flights)
-	return (
-		<div className="App">
-			<Header />
-			<section
-				className="Main-container"
-				style={{
-					backgroundImage: `url(${image})`,
-					backgroundPosition: 'center'
-				}}
-			>
-				<div className="search-flight">
-					<p>Search Flight</p>
-				</div>
-				<aside className="Search-section">
-					{/* <SearchForm setFlights={setFlights} /> */}
-          <BookingPage/>
-				</aside>
-			</section>
-		</div>
-	)
-}
+  const [flights, setFlights] = useState([]);
+  console.log("========================FLIGHTS================");
+  console.log(flights);
+  return (
+    <div className="App">
+      <Header />
+      <section className="Main-container" style={{ backgroundImage: `url(${image})`, backgroundPosition: "center" }}>
+        <div className="search-flight">
+          <p>Search Flight</p>
+        </div>
+        <aside className="Search-section">
+          <SearchForm setFlights={setFlights} />
+        </aside>
+      </section>
+      <section className="Results-section">
+        <FlightInfo />
+      </section>
+    </div>
+  );
+};
