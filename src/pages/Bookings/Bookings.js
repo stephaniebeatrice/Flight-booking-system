c6977a714ff174489dc1ad381389492f1f3c9ce9
Kevo-gmm <61180578+Kevo-gmm@users.noreply.github.com>
2023-07-20 19:05:39 +0300

    booking page

---
+++ src/pages/Bookings/Bookings.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../../components/header/header";
+
+export const Bookings = props => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
