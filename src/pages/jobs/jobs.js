a5ff02cdbd3a33be59e8e31c4294583a4a19b6ce
CarolMainaa <carolmaina502@gmail.com>
2023-07-21 19:24:20 +0300

    job page

--- src/pages/jobs/jobs.js
+++ src/pages/jobs/jobs.js
@@ -1,9 +1,91 @@
 import React from "react";
 import { Header } from "../../components/header/header";
 import "./style.css";
-import { JobCard } from "../../components/rows";
+import handlers from "../../img/handlers.jpg";
+import clearance from "../../img/clearance.jpg";
+import catering from "../../img/catering.jpg";
+import hostess from "../../img/hostess.jpg";
+import mechanic from "../../img/mechanic.jpg";
+import operations from "../../img/operations.jpg";
+import pilot from "../../img/pilot.jpg";
+import controllers from "../../img/controllers.jpg";
+import support from "../../img/support.jpg";
 
 export const Jobs = () => {
+
+  const jobData = [
+    {
+      id: 1,
+      title: "Airline Baggage handler",
+      subtitle:
+        "Responsibilities include transferring luggage from check-in areas to departure areas, moving luggage to and from the aircraft.",
+      details: ["Full Time", "Min. 2 Years", "Senior Level"],
+      imageUrl: handlers,
+    },
+    
+    {
+      id: 2,
+      title: "Passenger assistant",
+      subtitle: "A passenger assistant is a customer service professional who will help assist travelers at the airport board the right aeroplane as indicated in  the booking pass.",
+       details: ["Full Time", "Min. 1 Year", "Junior Level"],
+       imageUrl: operations ,
+    },
+
+    {
+      id: 3,
+      title: "Airline Clearance agents",
+      subtitle: " Airline Clearance agents verify booking passes at check-ins and onboarding.They also verify any other required documents eg passports.",
+       details: ["Part Time", "Min. 2 Years", "Junior Level"],
+       imageUrl: clearance,
+    },
+    {
+      id: 4,
+      title: "Pilots",
+      subtitle: "Pilots are responsible for transporting people and cargo,assessing and adjusting flight paths and updating passengers on conditions or delays.",
+      details: ["Full Time", "Min. 1 Year", "Senior Level"],
+       imageUrl: pilot,
+    },
+
+    {
+      id: 5,
+      title: "Flight attendant",
+      subtitle: "They ensure passengers' comfort and safety when in flight by demonstrating safety features and responding to any emergencies on the flight.",
+       details: ["Part Time", "Min. 1 Year", "Senior Level"],
+       imageUrl: hostess,
+    },
+    {
+      id: 6,
+      title: "Airline Caterers",
+      subtitle: " They are responsible for providing customer service and preparing meals for visitors in an airport and cleaning flight kitchen utensils. ",
+      details: ["Full Time", "Min. 1 Year", "Junior Level"],
+       imageUrl: catering,
+    },
+
+    {
+      id: 7,
+      title: "Avionics technician",
+      subtitle: "Avionics technicians are responsible for installing, repairing and maintaining equipment related to airplanes, helicopters and other fixed-wing aircraft.",
+      details: ["Full Time", "Min. 1 Year", "Senior Level"],
+       imageUrl: mechanic,
+    },
+    
+    {
+      id: 8,
+      title: "Air traffic controller",
+      subtitle: "Their duties include communicating with pilots, issuing take-off and landing instructions, monitoring aircrafts using computers programs and radar .",
+      details: ["Full Time", "Min. 1 Year", "Junior Level"],
+       imageUrl: controllers,
+    },
+    {
+      id: 9,
+      title: "Customer service agents",
+      subtitle:"These agents work at airport counters and  information desks. They help passengers their flight reservations and assist travelers with information regarding the airline.",
+      details: ["Remote", "Min. 1 Year", "Junior Level"],
+       imageUrl: support,
+    },
+
+  ];
+
   return (
     <div className="App">
       <Header />
@@ -60,12 +142,30 @@ export const Jobs = () => {
               </div>
             </div>
             <div className="searched-jobs">
-              <div className="job-cards">
-                {[1,2,3].map((item, index) => {
-                  // return <JobCard />;
-                   return <JobCard key={index} title={`Job ${item}`} description={`Description of Job ${item}`} />;
-
-                })}
+            <div className="job-cards">
+                {jobData.map((job) => (
+                  <div className="job-card" key={job.id}>
+                    <div className="job-card-header">
+                      {/* <div>{img}</div> */}
+                      <img src={job.imageUrl}  />
+                    </div>
+                    <div className="job-card-title">{job.title}</div>
+                    <div className="job-card-subtitle">{job.subtitle}</div>
+                    <div className="job-detail-buttons">
+                      {job.details.map((detail, index) => (
+                        <button className="search-buttons detail-button" key={index}>
+                          {detail}
+                        </button>
+                      ))}
+                    </div>
+                    <div className="job-card-buttons">
+                      <button className="search-buttons card-buttons">Apply Now</button>
+                      <button className="search-buttons card-buttons-msg">Messages</button>
+                    </div>
+                  </div>
+                ))}
+            </div>
+  
               </div>
               <div className="job-overview">
                 <div className="job-overview-cards">
@@ -478,6 +578,6 @@ export const Jobs = () => {
           </div>
         </div>
       </div>
-    </div>
+    // </div>
   );
 };
