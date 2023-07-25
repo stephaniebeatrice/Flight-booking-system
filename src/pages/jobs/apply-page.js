f5b39fab3819ceebc572d73cf853b32181cdfef0
CarolMainaa <carolmaina502@gmail.com>
2023-07-26 01:36:50 +0300

    style changes

--- src/pages/jobs/apply-page.js
+++ src/pages/jobs/apply-page.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { Header } from "../../components/header/header";
+import { useState } from "react";
 import "./style.css";
 
 export const Application = () => {
@@ -7,14 +8,14 @@ export const Application = () => {
   const [fileIsValid, setFileIsValid] = useState(true);
   const [submissionSuccess, setSubmissionSuccess] = useState(false);
 
-  const handleFileChange = event => {
+  const handleFileChange = (event) => {
     const file = event.target.files[0];
     setSelectedFile(file);
     setFileIsValid(validateFile(file));
   };
 
   // Validation function for allowed file extensions
-  const validateFile = file => {
+  const validateFile = (file) => {
     if (!file) {
       return true; // No file selected, consider as valid
     }
@@ -34,149 +35,116 @@ export const Application = () => {
   return (
     <div className="App">
       <Header />
-      {/* <div className="job">
-        <div className="job-wrapper">
-          <div className="main-container"> */}
-      {/* <div className="form-content"> */}
-      <div className="form-content">
-      <div id="psnlInfo" className="booking-tab-content active">
-        <div className="tab-wrapper">
-      <div class="payment-wrapper">
-        <div class="tab-wrapper">
-        <h2>Job Application Form</h2>
-            <div className="fieldset third">
-              <h2>Job Application Form</h2>
-              <div className="field">
-                <label id="lbl_rndTripTo" aria-label="To">
-                  First Name*
-                </label>
+
+      <div class="container">
+        <div class="apply_box">
+          <h1>Job Application Form</h1>
+          <form action="">
+            <div class="form_container">
+              <div class="form_field">
+                <label for="first_name"> First Name </label>
                 <input
-                  name="firstName"
-                  type="text"
-                  id="rndTripTo"
+                  id="first_name"
+                  name="first_name"
                   className="form-control"
                   placeholder="John"
-                  style={{ width: "100%" }}
-                  data-select2-id="rndTripTo"
+                />
+              </div>
+              <div class="form_field">
+                <label for="last_name"> Last Name </label>
+                <input
+                  id="last_name"
+                  name="last_name"
+                  className="form-control"
+                  placeholder="Doe"
                 />
               </div>
 
-              <div className="field">
-                <label id="lbl_rndTripTo" aria-label="To">
-                  Last Name*
-                </label>
+              <div className="form_field">
+                <label for="DateOfBirth"> Date of Birth</label>
                 <input
-                  name="lastName"
-                  type="text"
-                  id="rndTripTo"
+                  name="DateOfBirth"
+                  type="date"
                   className="form-control"
-                  placeholder="Doe"
-                  style={{ width: "100%" }}
-                  data-select2-id="rndTripTo"
+                  placeholder="mm-dd-yyyy"
                 />
               </div>
-              <div className="field date-field">
-                <label id="lbl_rndTripDates" aria-label="Date">
-                  Date of Birth*
-                </label>
-                <input name="DateOfBirth" type="date" className="form-control" placeholder="mm-dd-yyyy" style={{ width: "100%" }} />
-              </div>
-            </div>
-            <div className="fieldset half">
-              <div className="field">
-                <label id="lbl_rndTripPromoCode" aria-label="Promo Code">
-                  Phone number
-                </label>
+
+              <div class="form_field">
+                <label for="phoneNumber"> Phone Number </label>
                 <input
-                  name="phoneNumber"
                   type="tel"
-                  id="rndTripPromoCode"
-                  autoComplete="off"
+                  id="phoneNumber"
+                  name="phoneNumber"
                   className="form-control"
-                  aria-labelledby="lbl_rndTripPromoCode"
-                />
-              </div>
-              <div className="field">
-                <label id="lbl_rndTripPromoCode" aria-label="Promo Code">
-                  Email
-                </label>
-                <input
-                  name="email"
-                  type="email"
-                  id="rndTripPromoCode"
-                  placeholder="eg. johndoe@gmail.com"
-                  autoComplete="off"
-                  className="form-control"
-                  aria-labelledby="lbl_rndTripPromoCode"
-                />
-              </div>
-            </div>
-            <div className="fieldset half">
-              <div className="field">
-                <label id="lbl_rndTripTo" aria-label="To">
-                  City
-                </label>
-                <input
-                  name="firstName"
-                  type="text"
-                  id="rndTripTo"
-                  className="form-control"
-                  placeholder="John"
-                  style={{ width: "100%" }}
-                  data-select2-id="rndTripTo"
+                  placeholder=" +254 712 345 678"
                 />
               </div>
 
-              <div className="field">
-                <label id="lbl_rndTripTo" aria-label="To">
-                  Current Address*
-                </label>
+              <div class="textarea_control">
+                <label for="email"> Email </label>
                 <input
-                  name="lastName"
-                  type="text"
-                  id="rndTripTo"
+                  type="email"
+                  id="email"
+                  name="email"
                   className="form-control"
-                  placeholder="Doe"
-                  style={{ width: "100%" }}
-                  data-select2-id="rndTripTo"
+                  placeholder="johndoe@example.com"
                 />
               </div>
-            </div>
-            <div className="fieldset third">
-              <div className="field">
-                <label class="expiration-year">Position Applying for*</label>
-                <select id="experation-year" style={{ width: "100%" }} className="form-control" required>
-                  <option value="">Airline Baggage handler</option>
+
+              <div class="form_field">
+                <label for="city"> City </label>
+                <input
+                  id="city"
+                  className="form-control"
+                  name="city"
+                  placeholder="Nairobi"
+                />
+              </div>
+
+              <div class="form_field">
+                <label for="address"> Address </label>
+                <input
+                  id="address"
+                  name="address"
+                  className="form-control"
+                  placeholder="11111"
+                />
+              </div>
+
+              <div class="form_field">
+                <label for="job_role"> Job Role </label>
+                <select id="job_role" name="job_role">
+                <option value="">Flight attendant</option>
+                  <option value="">Baggage handler</option>
                   <option value="">Passenger assistant</option>
-                  <option value="">Airline Clearance agents</option>
-                  <option value="">Pilots</option>
-                  <option value="">Flight attendant</option>
+                  <option value="">Airline Clearance agent</option>
+                  <option value="">Pilot</option>
                   <option value="">Airline Caterers</option>
                   <option value="">Avionics technician</option>
                   <option value="">Air traffic controller</option>
-                  <option value="">Customer service agents</option>
+                  <option value="">Customer service agent</option>
                 </select>
               </div>
-              <div className="field date-field">
-                <label id="lbl_rndTripDates" aria-label="Date">
-                  Availabe Start Date*
-                </label>
-                <input name="DateOfBirth" type="date" className="form-control" placeholder="mm-dd-yyyy" style={{ width: "100%" }} />
+
+              <div class="form_field">
+                <label for="date"> Available start Date </label>
+                <input
+                  name="DateOfBirth"
+                  type="date"
+                  className="form-control"
+                  placeholder="mm-dd-yyyy"
+                />
               </div>
-              <div className="field">
-                <label class="expiration-year">What is your current employment status*</label>
-                <select id="experation-year" style={{ width: "100%" }} className="form-control" required>
-                  <option value="">Employed</option>
-                  <option value="">Self-Employed</option>
-                  <option value="">Unemployed</option>
-                  <option value="">Student</option>
-                </select>
-              </div>
-            </div>
-            <div className="field">
-              <label class="expiration-year">Upload your resume/CV*</label>
-              <div className="resume">
-                <input type="file" onChange={handleFileChange} />
+              <div class="form_field">
+                <label for="upload"> Upload your resume/CV* </label>
+                {/* <div className="resume"> */}
+                <input
+                  type="file"
+                  id="upload"
+                  name="upload"
+                  onChange={handleFileChange}
+                />
 
                 {!fileIsValid && (
                   <div>
@@ -184,24 +152,17 @@ export const Application = () => {
                     <p>Please upload a valid PDF or DOCX file.</p>
                   </div>
                 )}
-                {/* {selectedFile && fileIsValid && submissionSuccess && (
-                  <div>
-                    Submission success message
-                    <p>Submission successful!</p>
-                  </div>
-                )} */}
-              </div>
-              <div className="fieldset">
-                <button className="btn-submit" onClick={handleSubmit}>
-                  Submit
-                </button>
+                {/* </div> */}
               </div>
             </div>
-          </div>
+            <div class="button_container">
+              <button className="btn-submit" onClick={handleSubmit}>
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </div>
-     </div>
     </div>
   );
 };
