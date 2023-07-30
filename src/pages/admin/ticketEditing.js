92080c0ccc33e954eaaa69dce789114ea4aff362
stephaniebeatrice <beatriceladida@gmail.com>
2023-07-30 18:23:36 +0300

    Latest changes

--- src/pages/admin/ticketEditing.js
+++ src/pages/admin/ticketEditing.js
@@ -1,88 +1,112 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import './ticketEditing.css'
+import Sidebar from "./../admin/components/Sidebar";
+import Navbar from "./../admin/components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const TicketEditing = ({ passenger, onSave }) => {
-  const [firstName, setFirstName] = useState(passenger?.firstName || "");
-  const [lastName, setLastName] = useState(passenger?.lastName || "");
-  const [DOB, setDOB] = useState(passenger?.DOB || "");
-  const [flightDate, setFlightDate] = useState(passenger?.flightDate || "");
-  const [seatNumber, setSeatNumber] = useState(passenger?.seatNumber || "");
-  const [phoneNumber, setPhoneNumber] = useState(passenger?.phoneNumber || "");
-  const [email, setEmail] = useState(passenger?.email || "");
+	const [firstName, setFirstName] = useState(passenger?.firstName || '')
+	const [lastName, setLastName] = useState(passenger?.lastName || '')
+	const [DOB, setDOB] = useState(passenger?.DOB || '')
+	const [flightDate, setFlightDate] = useState(passenger?.flightDate || '')
+	const [seatNumber, setSeatNumber] = useState(passenger?.seatNumber || '')
+	const [phoneNumber, setPhoneNumber] = useState(passenger?.phoneNumber || '')
+	const [email, setEmail] = useState(passenger?.email || '')
 
-
-  const handleSave = () => {
-    const updatedPassenger = {
-      ...passenger,
-      firstName,
-      lastName,
-      DOB,
-      flightDate,
-      seatNumber,
-      phoneNumber,
-      email,
-    };
-    onSave(updatedPassenger);
+  const [toggle, setToggle] = useState(true);
+  const Toggle = () => {
+    setToggle(!toggle);
   };
 
-  return (
-    <div>
-      <h2>Edit Passenger Details</h2>
-      <form>
-        <label>First Name:</label>
-        <input
-          type="text"
-          value={firstName}
-          onChange={(e) => setFirstName(e.target.value)}
-        />
+	const handleSave = () => {
+		const updatedPassenger = {
+			...passenger,
+			firstName,
+			lastName,
+			DOB,
+			flightDate,
+			seatNumber,
+			phoneNumber,
+			email
+		}
+		onSave(updatedPassenger)
+	}
 
-        <label>Last Name:</label>
-        <input
-          type="text"
-          value={lastName}
-          onChange={(e) => setLastName(e.target.value)}
-        />
+	return (
+    <div className="container-fluid bg-primary min-vh-100 ">
+    <div className="row ">
+      {toggle && (
+        <div className="col-4 col-md-2 bg-white vh-100 position-fixed">
+          <Sidebar />
+        </div>
+      )}
+      {toggle && <div className="col-4 col-md-2"></div>}
+      <div className="col">
+        <Navbar Toggle={Toggle} />
+      </div>
+		<div className="form-container">
+			<h2>Edit Passenger Details</h2>
+			<form>
+				<div>
+					<label>First Name:</label>
+					<input
+						type="text"
+						value={firstName}
+						onChange={(e) => setFirstName(e.target.value)}
+					/>
 
-        <label>Date of Birth:</label>
-        <input
-          type="date"
-          value={DOB}
-          onChange={(e) => setDOB(e.target.value)}
-        />
+					<label>Last Name:</label>
+					<input
+						type="text"
+						value={lastName}
+						onChange={(e) => setLastName(e.target.value)}
+					/>
 
-        <label>Flight Date:</label>
-        <input
-          type="date"
-          value={flightDate}
-          onChange={(e) => setFlightDate(e.target.value)}
-        />
+					<label>Date of Birth:</label>
+					<input
+						type="date"
+						value={DOB}
+						onChange={(e) => setDOB(e.target.value)}
+					/>
+				</div>
+				<div>
+					<label>Flight Date:</label>
+					<input
+						type="date"
+						value={flightDate}
+						onChange={(e) => setFlightDate(e.target.value)}
+					/>
 
-        <label>Seat Number:</label>
-        <input
-          type="text"
-          value={seatNumber}
-          onChange={(e) => setSeatNumber(e.target.value)}
-        />
-
-        <label>Phone Number:</label>
-        <input
-          type="tel"
-          value={phoneNumber}
-          onChange={(e) => setPhoneNumber(e.target.value)}
-        />
-
-        <label>Email Address:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <button type="button" onClick={handleSave}>
-          Save
-        </button>
-      </form>
+					<label>Seat Number:</label>
+					<input
+						type="text"
+						value={seatNumber}
+						onChange={(e) => setSeatNumber(e.target.value)}
+					/>
+					<label>Phone Number:</label>
+					<input
+						type="tel"
+						value={phoneNumber}
+						onChange={(e) => setPhoneNumber(e.target.value)}
+					/>
+				</div>
+				<label>Email Address:</label>
+				<input
+					type="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<div>
+					<button type="button" onClick={handleSave}>
+						Save
+					</button>
+				</div>
+			</form>
+		</div>
     </div>
-  );
-};
+    </div>
+	)
+}
 
-export default TicketEditing;
+export default TicketEditing
