e1ab19c7349a4faf506dfe9a1d5478fe8065c041
stephaniebeatrice <beatriceladida@gmail.com>
2023-07-30 15:22:43 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import {
 	Routes
 } from 'react-router-dom'
 
-import { useSelector } from "react-redux";
-import { Bookings } from "./pages/Bookings/Bookings";
-import { Home } from "./pages/Home/home";
-import { SignUp } from "./pages/SignUp.js/SignUp";
-import Dashboard from "./pages/admin/components/Dashboard";
-import { BookingPage } from "./pages/booking/booking";
-import { Application } from "./pages/jobs/apply-page";
-import { Jobs } from "./pages/jobs/jobs";
-import { Login } from "./pages/login/Login";
-import { ticketEditing } from "./pages/admin/ticketEditing";
-import { Notification } from "./pages/admin/Notification";
-import EmployeeMatching from "./pages/admin/employeeMatching";
+import { useSelector } from 'react-redux'
+import { Bookings } from './pages/Bookings/Bookings'
+import { Home } from './pages/Home/home'
+import { SignUp } from './pages/SignUp.js/SignUp'
+import Dashboard from './pages/admin/components/Dashboard'
+import { BookingPage } from './pages/booking/booking'
+import { Application } from './pages/jobs/apply-page'
+import { Jobs } from './pages/jobs/jobs'
+import { Login } from './pages/login/Login'
+import TicketEditing from './pages/admin/ticketEditing'
+import { Notification } from './pages/admin/Notification'
+import EmployeeMatching from './pages/admin/employeeMatching'
 
-import { EditForm } from "./pages/Bookings/edit-form";
+import { EditForm } from './pages/Bookings/edit-form'
 
 const App = () => {
 	const user = useSelector((state) => state.authReducer.user)
@@ -43,7 +43,7 @@ const App = () => {
 				<Route path="/Admin" element={renderRestrictedComponent(Dashboard)} />
 				<Route
 					path="/TicketEdit"
-					element={renderRestrictedComponent(ticketEditing)}
+					element={renderRestrictedComponent(TicketEditing)}
 				/>
 				<Route
 					path="/Notification"
@@ -56,7 +56,7 @@ const App = () => {
 				<Route path="/Jobs" element={renderRestrictedComponent(Jobs)} />
 				<Route path="/Apply" element={renderRestrictedComponent(Application)} />
 				<Route path="/Bookings" element={renderRestrictedComponent(Bookings)} />
-        <Route path="/Edit" element={renderRestrictedComponent(EditForm)} />
+				<Route path="/Edit" element={renderRestrictedComponent(EditForm)} />
 			</Routes>
 		</Router>
 	)
